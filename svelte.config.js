ef22e164f1e25113f9b4382c3e7bb3b777770ad7
Michael Koeppl <koeppl.michael@outlook.com>
2022-07-15 20:42:13 +0200

    Update deployment for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -19,7 +21,15 @@ const config = {
       // This can be false if you're using a fallback (i.e. SPA mode)
       default: true
     }
-  }
+  },
+
+  // GitHub Pages specific stuff
+  paths: {
+    base: dev ? '' : '/rettetdemdativ.github.io',
+  },
+  // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+  // For example, instead of '_app', use 'app_', 'internal', etc.
+  appDir: 'internal',
 };
 
 export default config;
