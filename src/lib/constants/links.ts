191e73828bfb775ba1595f9d1121cdaf1c7cb503
Michael Koeppl <michael@koeppl.dev>
2023-12-13 23:59:48 +0100

    Update Mastodon profile links

--- src/lib/constants/links.ts
+++ src/lib/constants/links.ts
@@ -6,5 +6,5 @@ export const LINKS: TextData = {
   github: 'https://github.com/rettetdemdativ',
   stackoverflow: 'https://stackoverflow.com/users/2593209/calmandniceperson?tab=profile',
   twitter: 'https://twitter.com/rettetdemdativ',
-  mastodon: 'https://mastodon.online/web/@rettetdemdativ'
+  mastodon: 'https://wien.rocks/@rettetdemdativ'
 };
